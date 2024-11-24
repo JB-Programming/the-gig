bd10aa3c03877899caf34ab804b0b4eebf5b670c
Konstantin Preusser <konstantin.preusser@gmx.de>
2024-11-24 12:05:38 +0100

    database setup

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Hillmann_und_Geitz_DB',
         'USER': 'root',
-        'PASSWORD': 'Elina+35_elefanten!!tööö',
+        'PASSWORD': 'marschiboy4president',
         'HOST': 'localhost',
         'PORT': '3306',
     }
