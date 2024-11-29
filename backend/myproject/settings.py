5353ec3bfb735e2927938997e7841ef103143f7f
JB-Programming <jb.programming.21@gmail.com>
2024-11-29 15:01:50 +0100

    Changed PW

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Hillmann_und_Geitz_DB',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'marschiboy4president',
         'HOST': 'localhost',
         'PORT': '3306',
     }
